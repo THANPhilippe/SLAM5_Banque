9f9c759cf97f650313ae8705640afbe988e7e9de
Nhân THAN <philippethan@hotmail.com>
2017-01-04 12:04:42 +0100

    2nd commit

---
+++ banque/Compte.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banque
+{
+    class Compte
+    {
+        private int numero;
+  
+
+        public Compte(int numero)
+        {
+            this.numero = numero;
+        }
+
+    }
+
+    class Courant : Compte
+    {
+        int decouvert = 0;
+        int solde;
+        public Courant(int numero, int decouvert, int solde) : base(numero)
+        {
+        }
+
+    }
+
+    class Epargne : Compte
+    {
+
+        static int taux;
+        public Epargne(int numero) : base(numero)
+        {
+        }
+    }
+
+}
---
+++ banque/Personne.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace banque
+{
+    abstract class Personne
+    {
+        class Morale : Personne 
+        {
+            String raison_sociale;
+
+            public Morale(String raison_sociale): base()
+            {
+                this.raison_sociale = raison_sociale;
+
+            }
+        }
+
+        class Physique : Personne
+        {
+            String nom;
+            String prenom;
+            int revenu;
+
+            public Physique(String nom, String prenom, int revenu) : base()
+            {
+                this.nom = nom;
+                this.prenom = prenom;
+                this.revenu = revenu;
+            }
+
+        }
+    }
+}
